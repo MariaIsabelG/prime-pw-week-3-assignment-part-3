87da1099360aa204ec97af01a27071cb06c330d2
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-05-20 15:10:51 -0500

    added a new delivery to the supplyChanges array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -25,12 +25,16 @@ console.log('Second supply change is:', supplyChanges[1]);
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
-let removedChanges = supplyChanges.pop()
-console.log('Last item removed from supply changes:', removedChanges)
+let removedChanges = supplyChanges.pop();
+console.log('Last item removed from supply changes:', removedChanges);
 
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+supplyChanges.push(25);
+console.log('New delivery arrived. supplyChanges array is now:', supplyChanges);
+
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
