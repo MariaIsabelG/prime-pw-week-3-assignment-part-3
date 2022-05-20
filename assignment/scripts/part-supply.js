2e10d76631556f4457b9a87148f32523d937a554
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-05-20 17:56:14 -0500

    gave up on last 'while loop'

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -95,3 +95,4 @@ console.log(sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop')
+
