a83af72598d18c8bc8da5b02516979f301e4376e
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-05-20 15:07:52 -0500

    removed the last item in supplyChange array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -25,6 +25,10 @@ console.log('Second supply change is:', supplyChanges[1]);
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
+let removedChanges = supplyChanges.pop()
+console.log('Last item removed from supply changes:', removedChanges)
+
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 
